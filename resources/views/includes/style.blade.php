26c9f412807ff74d90229cd28d2abd763b26ae7e
arialghifari <arialghifari99@gmail.com>
2021-03-05 21:24:03 +0800

    add google fonts

--- resources/views/includes/style.blade.php
+++ resources/views/includes/style.blade.php
@@ -1,3 +1,5 @@
 <link rel="shortcut icon" href="{{ url('frontend/images/turi.ico') }}" type="image/x-icon" />
 <link rel="stylesheet" href="{{ url('frontend/styles/tailwind.css') }}" />
 <link rel="stylesheet" href="{{ url('frontend/styles/app.min.css') }}" />
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Assistant:wght@300;400;500;600;700;800&family=Playfair+Display:wght@700&display=swap" rel="stylesheet">
